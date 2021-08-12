ccd6d3d1e1b9019ef747059e9b910b85a4826e4d
HudaitRITAM <88477663+HudaitRITAM@users.noreply.github.com>
2021-08-12 09:22:54 +0530

    Add files via upload

---
+++ lab2/lab2_q11.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cout <<"Enter n : ";
+	cin >> n;
+	int a=n;
+	while(a>0)
+	{
+		a=a-5;
+		
+	}
+	if(a==0)
+	cout << n << " is divisible by 5"<< endl;
+	else
+	cout << n << " is NOT divisible by 5"<< endl;
+	
+}
+
